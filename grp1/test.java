97e2e1fae4712cba00cc0eccd9cd44c4e8c8db6d
FeSta <92328872+Datenflix007@users.noreply.github.com>
2024-12-02 11:41:31 +0100

    Update test.java
    added a new function

--- grp1/test.java
+++ grp1/test.java
@@ -1,5 +1,8 @@
 public class test{
-  public static void Main(String[] args){
+  public void printHello(){
     System.out.println("Hello WOrld!");
+  }
+  public static void Main(String[] args){
+    printHello();
   }
 }
